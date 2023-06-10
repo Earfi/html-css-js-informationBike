db91ccbd1324dbe7cd3a3aaac884e0b0ae6c1dd5
Pichaya Chantrasriwong <pichaya8442@gmail.com>
2023-06-10 17:39:14 +0700

    final

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # practice_informationBike_html_css_js
+
 practice_1
-| HTML | CSS | JAVASCRIPT     |
+| HTML | CSS | JAVASCRIPT |
 |-----:|-----------|-----------|
 
 | Name                  | Percentage |
@@ -8,14 +9,22 @@ practice_1
 | นายพิชญะ จันทรศรีวงศ์ |    100%    |
 
 ## Features
+
   <li> Go to Top Button</li>
   <li> Responsive </li>
   <li> Navbar </li>
+  <li> css scroll reveal </li>
 
 ## Design
+
 [Figma](https://www.figma.com/file/GTFWngiz1jEoFlU6yXqwEI/practice-html-css-js-%2F-informationBike?type=design&node-id=0%3A1&t=nKWsgcNWM44KRX4d-1)
-  
+
 <div style="display: flex;,flex-direction: row;">
   <img src="https://github.com/Earfi/practice_informationBike_html_css_js/assets/129359335/a11925bf-f1dc-4784-9f5e-bc8c4ad7769c" alt="image" style="width: 50%; max-width: 500px;">
+
   <img src="https://github.com/Earfi/practice_informationBike_html_css_js/assets/129359335/248d6520-9263-44c1-ba25-dcdee8960cc4" alt="image" style="width: 50%; max-width: 500px; margin-left: 10px;">
+
+  <img src="" alt="image" style="width: 50%; max-width: 500px; margin-left: 10px;">
 </div>
+
+![Alt text](image.png)
--- js/main.js
+++ js/main.js
@@ -26,3 +26,45 @@ const scrollUp = () => {
 }
 
 window.addEventListener('scroll',scrollUp)
+
+//  ================== scroll reveal animation =====================
+
+const sr = ScrollReveal({
+    origin:"top",
+    distance: '60px',
+    duration: 3000,
+    delay: 400,
+    reset: true //animations repeat
+})
+
+
+// main page
+sr.reveal(`.home_data,.home_card,.brand`,{delay:300})
+sr.reveal(`.home_card_title,.brands`,{delay:300,origin:'left'})
+
+
+// kawasaki page
+sr.reveal(`.bike`,{delay:300})
+sr.reveal(`.bike_brand`,{delay:300,origin:'right'})
+
+//introduction bike
+sr.reveal(`.intro_bike`,{delay:300})
+sr.reveal(`.intro_text`,{delay:300,origin:'left'})
+
+// detail bike
+sr.reveal(`.third_topic,.engine_img`,{delay:300})
+sr.reveal(`.main_h1,.main_img,.second_info,.third_engine`,{delay:300,origin:'left'})
+sr.reveal(`.main_info,.second_img`,{delay:300,origin:'right'})
+
+// bottom
+sr.reveal(`.footer`,{delay:300,origin:'bottom'})
+
+
+// sr.reveal(`.home_news_1,.home_news_2`,{delay:200,interval:150,origin:'right'})
+
+
+// sr.reveal(`.home__data,.explore__data,.explore__user`)
+// sr.reveal(`.home__card`,{delay:200,distance:'100px',interval:150})
+// sr.reveal(`.about__data,.join__image`,{origin:'right'})
+// sr.reveal(`.about__image,join__data`,{origin:'left'})
+// sr.reveal(`.popular__card`,{interval:150})
